c8c30012c0704f0263e4e4de6e2d79588dad5b47
Paul Cadman <paul.cadman@zuhlke.com>
2017-08-22 21:34:19 +0100

    Bunlde carthage 0.24 in the repository.

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd $SCRIPT_DIR/../ZeCamp && carthage bootstrap --platform ios --no-build
+cd $SCRIPT_DIR/../ZeCamp && $SCRIPT_DIR/../carthage/carthage bootstrap --platform ios --no-build
--- scripts/update.sh
+++ scripts/update.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd $SCRIPT_DIR/../ZeCamp && carthage update --platform ios --no-build
+cd $SCRIPT_DIR/../ZeCamp && $SCRIPT_DIR/../carthage/carthage update --platform ios --no-build
