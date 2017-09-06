2a575807d71582a0a70852884e27a11e5d2b4170
Mo Ramezanpoor <me@mohsenr.com>
2017-09-06 12:43:11 +0200

    Define Bundle.shortCommitId.

--- ZeCamp/ZeCamp/AppDelegate.swift
+++ ZeCamp/ZeCamp/AppDelegate.swift
@@ -6,7 +6,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        NSLog("Starting ZeCamp, git commit: " + Bundle.main.gitVersionString!)
+        NSLog("Starting ZeCamp, git commit: " + Bundle.main.shortCommitId!)
         if let buddybuildNumber = Bundle.main.buddybuildBuildNumber {
             NSLog("BuddyBuild build number %i", buddybuildNumber)
         } else {
--- ZeCamp/ZeCamp/Extensions/Bundle+gitVersionString.swift
+++ ZeCamp/ZeCamp/Extensions/Bundle+gitVersionString.swift
@@ -1,15 +1,22 @@
 import Foundation
 
 extension Bundle {
-    var gitVersionString: String? {
-        guard let gitCommit = self.infoDictionary?["GIT_COMMIT"] as? String,
-              let gitStatus = self.infoDictionary?["GIT_STATUS"] as? String else {
+    var commitId: String? {
+        guard
+            let gitCommit = self.infoDictionary?["GIT_COMMIT"] as? String,
+            let gitStatus = self.infoDictionary?["GIT_STATUS"] as? String else {
                 return nil
         }
         
         return "\(gitCommit) (\(gitStatus))"
     }
     
+    var shortCommitId: String? {
+        return commitId.map {
+            return String($0.prefix(8))
+        }
+    }
+
     var buddybuildBuildNumber: Int? {
         return self.infoDictionary?["BUDDYBUILD_BUILD_NUMBER"] as? Int
     }
