c64d3c9f671e4c6b419b86a061a63cff625e6e40
Moorcroft, Neil <Neil.Moorcroft@zuhlke.com>
2017-09-21 11:43:04 +0100

    changed to subtitle cell style

--- ZeCamp/ZeCamp/Sources/AppDelegate.swift
+++ ZeCamp/ZeCamp/Sources/AppDelegate.swift
@@ -10,7 +10,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         let window = UIWindow(frame: UIScreen.main.bounds)
         window.makeKeyAndVisible()
         self.window = window
-        window.rootViewController = UIViewController()
         
         let scheduleUrl = Bundle.main.bundleURL.appendingPathComponent("Content").appendingPathComponent("schedule.json")
         
