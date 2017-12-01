0e0aca77a9ab4871d94bea38f3ab2db6944ddd1d
Mo Ramezanpoor <me@mohsenr.com>
2017-12-01 15:20:41 +0000

    Define `Loadable`.

--- ZeCamp/ZeCamp/Sources/AppDelegate.swift
+++ ZeCamp/ZeCamp/Sources/AppDelegate.swift
@@ -22,27 +22,40 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         let indicator = UIActivityIndicatorView()
         indicator.startAnimating()
         vc.view = indicator
-        window.rootViewController = vc
         
-        model.schedule.observeOn(MainScheduler.instance).subscribe(onNext: { schedule in
-            let scheduleScreen = ScheduleScreen(schedule: schedule)
-            
-            UIBarButtonItem.appearance().setTitleTextAttributes([
-                .font: UIFont(name: "AAZuehlke", size: 18)!
-                ], for: .normal)
-            
-            let navigation = UINavigationController(rootViewController: scheduleScreen.makeViewController())
-            
-            navigation.navigationBar.prefersLargeTitles = true
-            navigation.navigationBar.largeTitleTextAttributes = [
-                .font: UIFont(name: "AAZuehlkeMedium", size: 28)!,
-            ]
-            
-            navigation.navigationBar.titleTextAttributes = [
-                .font: UIFont(name: "AAZuehlkeMedium", size: 18)!,
-            ]
-            
-            window.rootViewController = navigation
+        let rootViewController = model.schedule.observeOn(MainScheduler.instance).map { scheduleLoadable -> UIViewController in
+            switch scheduleLoadable {
+            case .loading:
+                let vc = UIViewController()
+                let indicator = UIActivityIndicatorView()
+                indicator.startAnimating()
+                vc.view = indicator
+                return vc
+                
+            case .loaded(let schedule):
+                let scheduleScreen = ScheduleScreen(schedule: schedule)
+                
+                UIBarButtonItem.appearance().setTitleTextAttributes([
+                    .font: UIFont(name: "AAZuehlke", size: 18)!
+                    ], for: .normal)
+                
+                let navigation = UINavigationController(rootViewController: scheduleScreen.makeViewController())
+                
+                navigation.navigationBar.prefersLargeTitles = true
+                navigation.navigationBar.largeTitleTextAttributes = [
+                    .font: UIFont(name: "AAZuehlkeMedium", size: 28)!,
+                ]
+                
+                navigation.navigationBar.titleTextAttributes = [
+                    .font: UIFont(name: "AAZuehlkeMedium", size: 18)!,
+                ]
+                
+                return navigation
+            }
+        }
+        
+        rootViewController.subscribe(onNext: { viewController in
+            window.rootViewController = viewController
         }).disposed(by: bag)
         
         return true
--- ZeCamp/ZeCamp/Sources/Model/AppModel.swift
+++ ZeCamp/ZeCamp/Sources/Model/AppModel.swift
@@ -7,15 +7,15 @@ struct AppModel {
         case missingScheduleFile
     }
     
-    var schedule: Observable<Schedule>
+    var schedule: Observable<Loadable<Schedule>>
     
     init(resourceProvider: ResourceProvider) {
         schedule = resourceProvider.load(contentsOf: .schedule).map { data in
             let decoder = JSONDecoder()
             decoder.dateDecodingStrategy = .iso8601
             
-            return try decoder.decode(Schedule.self, from: data)
-        }
+            return .loaded(try decoder.decode(Schedule.self, from: data))
+        }.startWith(.loading)
     }
     
 }
