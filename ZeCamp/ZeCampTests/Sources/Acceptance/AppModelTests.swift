d87cff2c7bc1df790df55edbbe3dc5dbb58dd331
Mo Ramezanpoor <me@mohsenr.com>
2017-12-01 16:42:27 +0000

    Implement introductory acceptance tests for AppModelTests.

---
+++ ZeCamp/ZeCampTests/Sources/Acceptance/AppModelTests.swift
@@ -0,0 +1,35 @@
+import XCTest
+import RxBlocking
+@testable import ZeCamp
+
+class AppModelTests: XCTestCase {
+    
+    func testFailingToLoadSchedule() {
+        let resourceProvider = ResourceProvider(folder: "NonExistentContent", in: Bundle(for: type(of: self)))
+        let model = AppModel(resourceProvider: resourceProvider)
+        
+        let result = model.schedule.toBlocking(timeout: 1).materialize()
+        
+        switch result {
+        case .completed(_):
+            XCTFail("Should not complete")
+        case .failed(let elements, _):
+            XCTAssertEqual(elements.count, 1)
+        }
+    }
+    
+    func testLoadingSchedule() {
+        let resourceProvider = ResourceProvider(folder: "Content", in: Bundle(for: type(of: self)))
+        let model = AppModel(resourceProvider: resourceProvider)
+        
+        let result = model.schedule.toBlocking(timeout: 1).materialize()
+        
+        switch result {
+        case .completed(let elements):
+            XCTAssertEqual(elements.count, 2)
+        case .failed(let elements, _):
+            XCTFail("Should not fail")
+        }
+    }
+    
+}
